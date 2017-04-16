248a1053dd41b4554aad73d9449b1fcd223ebec5
Sampath Vanimisetti <sampath.vanimisetti@gmail.com>
2017-04-16 08:06:29 +0530

    Add dropout to training pipeline

--- model.py
+++ model.py
@@ -5,7 +5,7 @@
 import csv
 
 lines = []
-with open('training-data\\driving_log_track1.csv') as csvfile:
+with open('training-data\\driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
@@ -78,7 +78,7 @@ validation_generator = generator(X_valid, y_valid, batch_size=32)
 
 from keras import backend as kbe
 from keras.models import Sequential, Model, load_model, model_from_json
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Dropout
 from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -88,15 +88,20 @@ model = Sequential()
 model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+#model.add(MaxPooling2D(pool_size=(2, 2),border_mode="same"))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+#model.add(MaxPooling2D(pool_size=(2, 2),border_mode="same"))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+#model.add(MaxPooling2D(pool_size=(2, 2),border_mode="same"))
 model.add(Convolution2D(64,3,3,activation="relu"))
+#model.add(MaxPooling2D(pool_size=(2, 2),border_mode="same"))
 model.add(Convolution2D(64,3,3,activation="relu"))
-#model.add(MaxPooling2D())
-#model.add(Dropout(0.1))
+#model.add(MaxPooling2D(pool_size=(2, 2),border_mode="same"))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(0.2))
 model.add(Dense(50))
+model.add(Dropout(0.2))
 model.add(Dense(10))
 model.add(Dense(1))
 
@@ -128,20 +133,19 @@ model.fit(X_train, y_train, validation_data=(X_valid,y_valid),
 
 # <codecell>
 
-model.save('model.h5')
+model.save('model_dropout.h5')
 print("Saved model to disk")
 
 # <codecell>
 
 # Run model network creation step (without compile & fit) before loading model
-#model = load_model('model.h5')
-#print("Loaded model from disk")
+model = load_model('model_dropout.h5')
+print("Loaded model from disk")
 
 # <codecell>
 
-#runfile('drive.py', args='model.h5')
-#runfile('drive.py', args='model_track2.h5 video_track2')
+runfile('drive.py', args='model_dropout.h5 video_dropout')
 
 # <codecell>
 
-#runfile('video.py', args='video_track2 --fps 48')
+runfile('video.py', args='video_dropout --fps 48')
